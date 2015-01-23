b602708602058064986ec9ed056ec25b6d444b18
Erik Carlsson <info@code301.com>
2015-01-24 00:06:51 +0100

    Init

---
+++ lib/App/Map/Metro.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package App::Map::Metro;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use App::Map::Metro;
+
+=head1 DESCRIPTION
+
+App::Map::Metro is ...
+
+=head1 SEE ALSO
+
+=cut
